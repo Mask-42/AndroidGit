68bc55e7766d243681b4b0a3088ca8085dceb3f4
shubham-akshat <shubham.akshat13@gmail.com>
2017-07-11 05:00:08 +0530

    Help dialog added

--- app/src/main/java/com/example/manpr/gittest1/HelpActivity.java
+++ app/src/main/java/com/example/manpr/gittest1/HelpActivity.java
@@ -1,13 +1,26 @@
 package com.example.manpr.gittest1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
 
-public class HelpActivity extends AppCompatActivity {
-
+public class HelpActivity extends Activity implements DialogInterface.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("HELP");
+        String data="All copyright, trade marks, design rights, patents and other intellectual property rights (registered and unregistered) in and on SAM Online Services and SAM Content belong to the SAM and/or third parties (which may include you or other users.) The SAM reserves all of its rights in SAM Content and SAM Online Services. Nothing in the Terms grants you a right or license to use any trade mark, design right or copyright owned or controlled by the SAM or any other third party except as expressly provided in the Terms.";
+        alertDialog.setMessage(data+" ");
+        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE,"OK",HelpActivity.this);
+        alertDialog.show();
+    }
+
+    @Override
+    public void onClick(DialogInterface dialogInterface, int i) {
+        finish();
     }
 }
